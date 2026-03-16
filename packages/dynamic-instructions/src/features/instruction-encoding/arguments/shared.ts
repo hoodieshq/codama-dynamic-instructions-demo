228f181f16f882bc1da9183968d9458493d01227
Mikhail <mikhail@hoodies.company>
2026-03-16 11:51:11 +0300

    fix: replace optionalNode regex

--- packages/dynamic-instructions/src/features/instruction-encoding/arguments/shared.ts
+++ packages/dynamic-instructions/src/features/instruction-encoding/arguments/shared.ts
@@ -4,6 +4,7 @@ export function isOmittedArgument(node: InstructionArgumentNode) {
     return node.defaultValueStrategy === 'omitted';
 }
 
+const optionalNodeKinds = ['optionTypeNode', 'zeroableOptionTypeNode', 'remainderOptionTypeNode'];
 export function isOptionalArgument(ixArgumentNode: InstructionArgumentNode, input: unknown) {
-    return ixArgumentNode.type.kind.search(/optionTypeNode/i) !== -1 && (input === null || input === undefined);
+    return optionalNodeKinds.includes(ixArgumentNode.type.kind) && (input === null || input === undefined);
 }
