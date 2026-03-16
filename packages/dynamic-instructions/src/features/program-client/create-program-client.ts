0943f30b304c4011f4179d0c5b740d2f61283fb6
Mikhail <mikhail@hoodies.company>
2026-03-16 12:38:01 +0300

    fix: add ProgramClient constraint to createProgramClient

--- packages/dynamic-instructions/src/features/program-client/create-program-client.ts
+++ packages/dynamic-instructions/src/features/program-client/create-program-client.ts
@@ -50,7 +50,7 @@ export type ProgramMethodBuilder = {
  * const client = createProgramClient<MyProgramClient>(idl);
  * ```
  */
-export function createProgramClient<TClient = ProgramClient>(
+export function createProgramClient<TClient extends ProgramClient = ProgramClient>(
     idl: IdlInput,
     options: CreateProgramClientOptions = {},
 ): TClient {
